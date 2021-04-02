a48af511b42628258a76181c2fb56a7c5c6f18a3
SantiagoGutierrezB <sangb@live.com.mx>
2021-04-01 23:21:46 -0600

    creacón de  proyecto

---
+++ News/News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Santiago Gutierrez Barcenas on 4/1/21.
+//  Copyright © 2021 Lia Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
